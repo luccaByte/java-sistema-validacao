9d5b9db0f57088256a4cdc68b1b39eab03b00e09
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-10 20:37:57 +0000

    primeiro commit

---
+++ src/test/java/br/com/sistema_validacao/minerva/MinervaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema_validacao.minerva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
