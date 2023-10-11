0473a3acc3a825eebdec38681cd388edc0e51564
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-11 20:14:20 +0000

    estruturando um laço de repetição para selecionar os candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,12 +4,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("Processo seletivo");
         /*
+            System.out.println("Processo seletivo");
             analisarCandidato(1900.0);
             analisarCandidato(2200.0);
             analisarCandidato(2000.0);
          */
+
+        selecaoCandidatos();
     }
 
     static void selecaoCandidatos() {
@@ -18,11 +20,11 @@ public class ProcessoSeletivo {
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5) {
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
 
-            System.out.println("O candidato " + candidato + "Solicitou este valor de salário " + salarioPretendido);
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
             if (salarioBase >= salarioPretendido) {
                 System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
                 candidatosSelecionados++;
