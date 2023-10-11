1d51cd640db213bbb2944142d68937348e9539ba
lucas <lucaston2323@gmail.com>
2023-10-11 20:32:22 -0300

    criando uma estrutura de repetição dowhile

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,41 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         selecaoCandidatos();
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato (String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato Realizado com sucesso!");
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + "tentativa!");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + " número máximo de tentativas " + tentativasRealizadas + " realizada...");
+        }
+    }
+
+    // Método auxiliar
+    static boolean atender () {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados () {
