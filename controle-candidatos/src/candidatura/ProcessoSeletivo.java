d4a988d449612b1b3f377a69fd03d81394f64dda
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-11 20:50:47 +0000

    Imprimindo uma lista com os candidatos selecionados

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -14,11 +14,26 @@ public class ProcessoSeletivo {
         selecaoCandidatos();
     }
 
+    static void imprimirSelecionados () {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº: " + (indice+1) + " é o " + candidatos[indice]);
+        }
+        
+        System.out.println("Forma abreviada de interação for each");
+        for (String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
     static void selecaoCandidatos() {
         String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
 
         int candidatosSelecionados = 0;
-        int candidatoAtual = 0;
+        int candidatoAtual = 0; //contagem iniciando a partir do índice 0 (array)
         double salarioBase = 2000.0;
         while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos[candidatoAtual];
